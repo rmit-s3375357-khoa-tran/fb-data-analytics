a4a3ee99d7945d58b93769c70f69d68f84c53c82
s3330363 <s3330363@student.rmit.edu.au>
2017-08-08 05:21:27 +0000

    Twitter Data Collection Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,17 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <?php
+                    use Abraham\TwitterOAuth\TwitterOAuth;
+                    $CONSUMER_KEY = "JLbyO2CtUynqtNHJnhrNZ6PZl";
+                    $CONSUMER_SECRET = "i2OwurNBeCVbh6kw7tgysmOaDKt3RB8HYnmwRRtwDCCudzsjNV";
+                    $access_token = "894781214451617792-angr8pULQrjcKFLmu42U739Uv0hTpd3";
+                    $access_token_secret = "WC8v9XS5jJU2CV1yXYhEN21N6iUlHVamClR40HoF75Dsf";
+                    $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $access_token, $access_token_secret);
+                    $content = $connection->get("account/verify_credentials");
+                    $statuses = $connection->get("search/tweets", ["q" => "twitterapi"]);
+                    var_dump($statuses);
+                ?>
             </div>
         </div>
     </body>
