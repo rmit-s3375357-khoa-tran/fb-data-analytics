72f4ddbdf5ae61f8abbba9edf3a8801c4349407c
Nancy <s3510210@student.rmit.edu.au>
2017-08-22 15:49:19 +1000

    graph for sentiment analysis

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -1,11 +1,69 @@
 @extends('master')
 
 @section('content')
+
+
     <div class="content">
-        <p>Positive tweets {{ $sentiments['positive'] }}</p>
-        <p>Negative tweets {{ $sentiments['negative']  }}</p>
-        <p>>Neutral tweets {{ $sentiments['neutral']  }}</p>
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+
         <div class="title">Results for <em>{{ $keyword }}</em></div>
+
+        <div id="graph" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+        <script type="text/javascript">
+
+            Highcharts.chart('graph', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Social Media Sentiment Analysis'
+                },
+                xAxis: {
+                    categories: [
+                        'Twitter',
+                        'Facebook',
+                        'Youtube'
+                    ],
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'sentiment'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Positive',
+                    data: [{{ $sentiments['positive'] }}, 0, 0]
+
+                }, {
+                    name: 'Negative',
+                    data: [{{ $sentiments['negative']  }}, 0, 0]
+
+                }, {
+                    name: 'Neutral',
+                    data: [{{ $sentiments['neutral']  }}, 0, 0]
+
+                }]
+            });
+        </script>
+
         <table class="table-striped table-responsive">
             <thead>
             <tr>
