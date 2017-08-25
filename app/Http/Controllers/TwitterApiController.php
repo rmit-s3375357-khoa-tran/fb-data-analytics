268ba53689f024f5462565b368902472a7fd1b56
tommy <s3375357@student.rmit.edu.au>
2017-08-25 13:57:36 +1000

    google map

--- app/Http/Controllers/TwitterApiController.php
+++ app/Http/Controllers/TwitterApiController.php
@@ -14,6 +14,9 @@ class TwitterApiController extends Controller
 {
     private $twitter;
 
+    //API URL
+    const API_URL ='http://maps.googleapis.com/maps/api/geocode/json';
+
     /**
      * TwitterApiController constructor.
      *
@@ -31,6 +34,117 @@ class TwitterApiController extends Controller
     }
 
     /**
+     * Do call
+     *
+     * @return object
+     * @param  array  $parameters
+     */
+    protected function doCall($parameters = array())
+    {
+        // check if curl is available
+        if (!function_exists('curl_init')) {
+            // throw error
+            throw new GeolocationException('This method requires cURL (http://php.net/curl), it seems like the extension isn\'t installed.');
+        }
+
+        // define url
+        $url = self::API_URL . '?';
+
+        // add every parameter to the url
+        foreach ($parameters as $key => $value) $url .= $key . '=' . urlencode($value) . '&';
+
+        // trim last &
+        $url = trim($url, '&');
+
+        // init curl
+        $curl = curl_init();
+
+        // set options
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+        // execute
+        $response = curl_exec($curl);
+
+        // fetch errors
+        $errorNumber = curl_errno($curl);
+        $errorMessage = curl_error($curl);
+
+        // close curl
+        curl_close($curl);
+
+        // we have errors
+        if ($errorNumber != '') throw new GeolocationException($errorMessage);
+
+        // redefine response as json decoded
+        $response = json_decode($response);
+
+        // return the content
+        return $response->results;
+    }
+
+    /**
+     * Get address using latitude/longitude
+     *
+     * @return array(label, components)
+     * @param  float        $latitude
+     * @param  float        $longitude
+     */
+    public function getAddress($latitude, $longitude)
+    {
+        $addressSuggestions = $this->getAddresses($latitude, $longitude);
+
+        return $addressSuggestions[0];
+    }
+
+    /**
+     * Get possible addresses using latitude/longitude
+     *
+     * @return array(label, street, streetNumber, city, cityLocal, zip, country, countryLabel)
+     * @param  float        $latitude
+     * @param  float        $longitude
+     */
+    public function getAddresses($latitude, $longitude)
+    {
+        // init results
+        $addresses = array();
+
+        // define result
+        $addressSuggestions = $this->doCall(array(
+            'latlng' => $latitude . ',' . $longitude,
+            'sensor' => 'false'
+        ));
+
+        // loop addresses
+        foreach ($addressSuggestions as $key => $addressSuggestion) {
+            // init address
+            $address = array();
+
+            // define label
+            $address['label'] = isset($addressSuggestion->formatted_address) ?
+                $addressSuggestion->formatted_address : null
+            ;
+
+            // define address components by looping all address components
+            foreach ($addressSuggestion->address_components as $component) {
+                $address['components'][] = array(
+                    'long_name' => $component->long_name,
+                    'short_name' => $component->short_name,
+                    'types' => $component->types
+                );
+            }
+
+            $addresses[$key] = $address;
+        }
+
+        return $addresses;
+    }
+
+
+
+    /**
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -40,43 +154,55 @@ class TwitterApiController extends Controller
         $statuses = $this->twitter->get("search/tweets", ["q" => $keyword]);
         $results = $statuses->statuses;
 
+        //print_r($results);
+        // mapbox
+
+        // Geotag
+
+        // Some test coordinate
+        // Melbourne coordinates (37.8136,144.963)
+        // Ho Chi Minh city (10.8231,106.6297)
+        // RMIT Melbourne (-37.804663448 , 144.957996168)
+
+        $coordinate = $this->getAddress(-37.804663448 , 144.957996168);
+
+        $coordinate["lon"] = -37.804663448;
+        $coordinate["lat"] = 144.957996168;
+        echo 'Address = ' . $coordinate['label'] . '<br/>';
+
+
         /*****Analysing sentiments******/
         $sentiments = $this->sentimentAnalysis($results);
         $tweetSentiments = $this->tweetSentimentAnalysis($results);
 
-        return view('twitter', compact('keyword', 'results', 'sentiments', 'tweetSentiments'));
+        return view('twitter', compact('keyword', 'results', 'sentiments', 'tweetSentiments','coordinate'));
     }
 
 
+// ##### DatumboxAPI sentiment analysis #####
     private function tweetSentimentAnalysis($results)
     {
         require_once('DatumboxAPI.php');
 
         $DatumboxAPI = new DatumboxAPI(env('DATUM_BOX_API'));
-        $sentiment_counter = array("positive"=>0, "negative"=>0, "neutral"=>0);
+        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
 
         $analysis = array();
 
         echo "<br> DATUMBOX <br>";
-        foreach ($results as $index=>$result)
-        {
+        foreach ($results as $index => $result) {
             $message = $result->text;
             //$message = str_replace('@', "", $message);
             $analysis[$index + 1] = $DatumboxAPI->TwitterSentimentAnalysis($message);
             $value = $analysis[$index + 1];
 
-            echo "id: ".($index + 1)." value: ".$value."<br>";
+            echo "id: " . ($index + 1) . " value: " . $value . "<br>";
 
-            if ($value=="negative")
-            {
+            if ($value == "negative") {
                 $sentiment_counter['negative']++;
-            }
-            elseif ($value=="positive")
-            {
+            } elseif ($value == "positive") {
                 $sentiment_counter['positive']++;
-            }
-            else
-            {
+            } else {
                 $sentiment_counter['neutral']++;
             }
 
@@ -86,19 +212,19 @@ class TwitterApiController extends Controller
 
     }
 
+// ##### AzureAPI sentiment analysis #####
     private function sentimentAnalysis($results)
     {
 
         /* Converting messages into json body for request*/
         $body_message = '{ "documents" : [';
-        foreach ($results as $index=>$result)
-        {
+        foreach ($results as $index => $result) {
             $message = $result->text;
             $message = str_replace('"', "'", $message);
-            $id = $index +1;
+            $id = $index + 1;
             $body_message .= '{ "language": "en", 
-                                "id": '.$id.',
-                                "text": "'.$message.'"},';
+                                "id": ' . $id . ',
+                                "text": "' . $message . '"},';
         }
         $body_message .= "]}";
 
@@ -114,34 +240,28 @@ class TwitterApiController extends Controller
 
         /*Response returned*/
         $json = "";
-        if  ($res->getStatusCode() == 200)
-        {
+        if ($res->getStatusCode() == 200) {
             $json = $res->getBody();
         }
 
         /* Parsing results and incrementing a sentiment counter*/
-        $results = json_decode($json,true);
-        $sentiment_counter = array("positive"=>0, "negative"=>0, "neutral"=>0);
+        $results = json_decode($json, true);
+        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
 
-        echo "AZURE"."<br>";
-        foreach ($results['documents'] as $result){
-            $score =  $result['score'];
+        echo "AZURE" . "<br>";
+        foreach ($results['documents'] as $result) {
+            $score = $result['score'];
             $id = $result['id'];
 
-            if ($score < 0.5)
-            {
+            if ($score < 0.5) {
                 $sentiment_counter['negative']++;
-                echo "id: ".($id)." value: negative<br>";
-            }
-            elseif ($score > 0.5)
-            {
+                echo "id: " . ($id) . " value: negative<br>";
+            } elseif ($score > 0.5) {
                 $sentiment_counter['positive']++;
-                echo "id: ".($id)." value: positive<br>";
-            }
-            else
-            {
+                echo "id: " . ($id) . " value: positive<br>";
+            } else {
                 $sentiment_counter['neutral']++;
-                echo "id: ".($id)." value: neutral<br>";
+                echo "id: " . ($id) . " value: neutral<br>";
             }
             //echo $result['score']."<br>";
         }
@@ -155,4 +275,9 @@ class TwitterApiController extends Controller
 
         return $sentiment_counter;
     }
+
+
+
+
 }
+class GeolocationException extends \Exception {}
--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -1,15 +1,49 @@
 @extends('master')
 
 @section('content')
-
-
     <div class="content">
         <script src="https://code.highcharts.com/highcharts.js"></script>
         <script src="https://code.highcharts.com/modules/exporting.js"></script>
 
+
+
+
         <div class="title">Results for <em>{{ $keyword }}</em></div>
 
         <div id="graph" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+
+        <!-- Google Map API -->
+        <div id ="map"></div>
+
+        <script>
+            initMap();
+
+            function initMap(){
+                //var coordinate = JSON.parse({{ json_encode($coordinate) }});
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+
+                    center: new google.maps.LatLng({{$coordinate["lon"] , $coordinate["lat"]}}),
+                    zoom: 12
+                });
+
+
+//                var GraphCoordinate = new google.maps.LatLng(coordinate);
+//                var map = new google.maps.Map(document.getElementById('map'), {
+//                    center: GraphCoordinate,
+//                    zoom: 3
+//                });
+            }
+        </script>
+        {{--<script async defer src="https://maps.googleapis.com/maps/api/js?callback=initMap" type="text/javascript"></script>--}}
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZKtWWEz1mLZZKGl9jdLUFHbPL5nuY5AE&callback=initMap">
+
+        </script>
+
+
+        <!-- End Google Map API -->
 
         <script type="text/javascript">
 
@@ -64,6 +98,7 @@
             });
         </script>
 
+
         <table class="table-striped table-responsive">
             <thead>
             <tr>
@@ -74,18 +109,20 @@
             </tr>
             </thead>
             <tbody>
-                @foreach($results as $index=>$result)
-                    <tr>
-                        <td>{{ $index+1 }}</td>
-                        <?php
-                            $timeCarbon = new \Carbon\Carbon($result->created_at);
-                            $time = $timeCarbon->toDateTimeString();
-                        ?>
-                        <td>{{ $time }}</td>
-                        <td>{{ $result->lang }}</td>
-                        <td><div>{{ $result->text }}</div></td>
-                    </tr>
-                @endforeach
+            @foreach($results as $index=>$result)
+                <tr>
+                    <td>{{ $index+1 }}</td>
+                    <?php
+                    $timeCarbon = new \Carbon\Carbon($result->created_at);
+                    $time = $timeCarbon->toDateTimeString();
+                    ?>
+                    <td>{{ $time }}</td>
+                    <td>{{ $result->lang }}</td>
+                    <td>
+                        <div>{{ $result->text }}</div>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
