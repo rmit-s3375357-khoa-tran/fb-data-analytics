c6ffb7e3df71fb6cdcc01bf3d096d2b2f4745cb7
Grace Zheng <s3558319@student.rmit.edu.au>
2017-08-28 23:36:29 +1000

    integrated streaming and preprocessing without testing as python doesnt run 	modified:   .idea/workspace.xml 	modified:   app/Http/Controllers/TwitterApiController.php

--- app/Http/Controllers/TwitterApiController.php
+++ app/Http/Controllers/TwitterApiController.php
@@ -2,33 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
-
-use App\Http\Requests;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class TwitterApiController extends Controller
 {
-    private $twitter;
-
-    /**
-     * TwitterApiController constructor.
-     *
-     * Set up authorised connection to twitter api
-     */
-    public function __construct()
-    {
-        $this->twitter = new TwitterOAuth(
-            config('setting.twitter_oauth.customer_key'),
-            config('setting.twitter_oauth.customer_secret'),
-            config('setting.twitter_oauth.access_token'),
-            config('setting.twitter_oauth.access_token_secret')
-        );
-        $this->twitter->get("account/verify_credentials");
-    }
-
     /**
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
@@ -36,11 +14,63 @@ class TwitterApiController extends Controller
     public function search(Request $request)
     {
         $keyword = isset($request->keyword) ? $request->keyword : 'twitterapi';
-        $statuses = $this->twitter->get("search/tweets", ["q" => $keyword]);
-        $results = $statuses->statuses;
-        $process = new Process('python3 tweepyStream.py Tom');
+        $stopword = isset($request->stopword) ? $request->stopword : '';
+
+        $process = new Process('python3 ' . asset('tweepyStream.py') . ' ' . $keyword);
         $process->run();
-        dd($process->getOutput());
+        $results = json_decode($process->getOutput(), true);
+
+        $this->saveToCsvFile($results, "raw_data_for_".$keyword.".csv");
+        $results = $this->preprocess($results, $stopword);
+        $this->saveToCsvFile($results, "preprocessed_data_for_".$keyword.".csv");
+
         return view('twitter', compact('keyword', 'results'));
     }
+
+    private function saveToCsvFile($results, $filename)
+    {
+        $fp = fopen($filename, 'w');
+        // header for csv
+        $header = [
+            "created_at", "tweet", "user_location", "user_timezone", "geo", "place_coordinates"
+        ];
+
+        fputcsv($fp, $header);
+        
+        foreach ($results as $result)
+        {
+            $fields = $this->prepareFieldsForCsv($result);
+            fputcsv($fp, $fields);
+        }
+
+        fclose($fp);
+    }
+
+    private function prepareFieldsForCsv($result)
+    {
+        // only extra info needed
+        $fields = [
+            'created_at'        => $result->created_at,
+            'tweet'             => $result->text,
+            'user_location'     => $result->user->location,
+            'user_timezone'     => $result->user->time_zone,
+            'geo'               => $result->geo,
+            'place_longitude'   => isset($result->place) ? $result->place->bounding_box->coordinates[0][0][0] : null,
+            'place_latitude'    => isset($result->place) ? $result->place->bounding_box->coordinates[0][0][1] : null,
+        ];
+
+        return $fields;
+    }
+
+    private function preprocess($results, $stopword)
+    {
+        $processedData = [];
+
+        foreach ($results as $result)
+            if(strpos(strtolower($result->text), strtolower($stopword)) === false)
+                $processedData[] = $result;
+
+        return $processedData;
+    }
+
 }
