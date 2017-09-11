b906d49bfa08bf816c24d3e7c1f8ce6b0c45cb88
Grace Zheng <s3558319@student.rmit.edu.au>
2017-09-11 22:38:11 +1000

    refactor functions that can be used by other social media 	copied:     app/Http/Controllers/TwitterApiController.php -> app/Http/Controllers/ApiController.php 	modified:   app/Http/Controllers/TwitterApiController.php 	deleted:    public/preprocessed_data_for_food.csv 	deleted:    public/preprocessed_data_for_trump.csv 	deleted:    public/raw_data_for_food.csv 	deleted:    public/raw_data_for_pauline hanson.csv 	deleted:    public/raw_data_for_trump.csv

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+
+class ApiController extends Controller
+{
+    // ##### DatumboxAPI sentiment analysis #####
+    protected function analyseTweet($results)
+    {
+        require_once('DatumboxAPI.php');
+        $DatumboxAPI = new DatumboxAPI(env('DATUM_BOX_API2'));
+        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
+        //$location = array();
+        $positiveLocation = array();
+        $negativeLocation = array();
+        $neutralLocation = array();
+
+        //echo "<br> DATUMBOX <br>";
+        foreach ($results as $index => $result) {
+            $message = $DatumboxAPI->TwitterSentimentAnalysis(str_replace('@', "", $result['tweet']));
+            //echo "id: " . ($index + 1) . " value: " . $message . "<br>";
+
+            if ($message == "negative") {
+                $sentiment_counter['negative']++;
+            } elseif ($message == "positive") {
+                $sentiment_counter['positive']++;
+            } else {
+                $sentiment_counter['neutral']++;
+            }
+
+
+            // Get lat and long by address
+            $address = $result['user_timezone']; // Google HQ
+            if ($address != null) {
+                $prepAddr = str_replace(' ', '+', $address);
+                $geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . $prepAddr . '&sensor=false');
+                $output = json_decode($geocode);
+                $latitude = $output->results[0]->geometry->location->lat;
+                $longitude = $output->results[0]->geometry->location->lng;
+                //array_push($location,array("lat"=>$latitude,"lng"=>$longitude));
+                if ($message == "negative") {
+                    array_push($negativeLocation, array("lat" => $latitude, "lng" => $longitude));
+                } elseif ($message == "positive") {
+                    array_push($positiveLocation, array("lat" => $latitude, "lng" => $longitude));
+                } else {
+                    array_push($neutralLocation, array("lat" => $latitude, "lng" => $longitude));
+                }
+            }
+        }
+
+        return [$sentiment_counter, $negativeLocation,$positiveLocation,$neutralLocation];
+
+    }
+
+    // ##### AzureAPI sentiment analysis #####
+    protected function sentimentAnalysis($results)
+    {
+        $positiveLocation = array();
+        $negativeLocation = array();
+        $neutralLocation = array();
+
+        /* Converting messages into json body for request*/
+        $body_message = '{ "documents" : [';
+        foreach ($results as $index => $result) {
+            $message = $result['tweet'];
+            $message = str_replace('"', "'", $message);
+            $id = $index + 1;
+            $body_message .= '{ "language": "en", 
+                                "id": ' . $id . ',
+                                "text": "' . $message . '"},';
+        }
+        $body_message .= "]}";
+
+        /*Creating a request*/
+        $client = new Client(); //GuzzleHttp\Client
+        $res = $client->request('POST', 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', [
+            'headers' => [
+                'content-type' => 'application/json',
+                'Ocp-Apim-Subscription-Key' => env('AZURE_KEY_2')
+            ],
+            'body' => $body_message
+        ]);
+
+        /*Response returned*/
+        $json = "";
+        if ($res->getStatusCode() == 200) {
+            $json = $res->getBody();
+        }
+
+        /* Parsing results and incrementing a sentiment counter*/
+        $return = json_decode($json, true);
+        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
+
+        //echo "AZURE" . "<br>";
+        foreach ($return['documents'] as $result) {
+            $score = $result['score'];
+
+            if ($score < 0.5) {
+                $sentiment_counter['negative']++;
+            } elseif ($score > 0.5) {
+                $sentiment_counter['positive']++;
+            } else {
+                $sentiment_counter['neutral']++;
+            }
+
+            // Get lat and long by address
+            $index = $result['id'] - 1;
+            $address = $results[$index]['user_timezone']; // Google HQ
+            if ($address != null) {
+                $prepAddr = str_replace(' ', '+', $address);
+                $geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . $prepAddr . '&sensor=false');
+                $output = json_decode($geocode);
+                $latitude = $output->results[0]->geometry->location->lat;
+                $longitude = $output->results[0]->geometry->location->lng;
+                //array_push($location,array("lat"=>$latitude,"lng"=>$longitude));
+                if ($score < 0.5) {
+                    array_push($negativeLocation, array("lat" => $latitude, "lng" => $longitude));
+                } elseif ($score > 0.5 == "positive") {
+                    array_push($positiveLocation, array("lat" => $latitude, "lng" => $longitude));
+                } else {
+                    array_push($neutralLocation, array("lat" => $latitude, "lng" => $longitude));
+                }
+            }
+
+        }
+
+        return [$sentiment_counter, $negativeLocation,$positiveLocation,$neutralLocation];
+
+    }
+
+    protected function saveToCsvFile($results, $filename)
+    {
+        $fp = fopen($filename, 'w');
+        // header for csv
+        $header = [
+            "created_at", "tweet", "user_location", "user_timezone", "geo", "place_coordinates"
+        ];
+
+        fputcsv($fp, $header);
+
+        foreach($results as $result)
+            fputcsv($fp, $result);
+
+        fclose($fp);
+    }
+
+    protected function preprocess($results, $stopwords)
+    {
+        $processedData = [];
+
+        foreach ($results as $result)
+        {
+            $hasStopWord = false;
+
+            foreach ($stopwords as $stopword)
+            {
+                if(strpos(strtolower($result['tweet']), strtolower($stopword)) !== false)
+                    $hasStopWord = true;
+            }
+
+            if(! $hasStopWord )
+                $processedData[] = $result;
+        }
+
+        return $processedData;
+    }
+}
--- app/Http/Controllers/TwitterApiController.php
+++ app/Http/Controllers/TwitterApiController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client;
-
-class TwitterApiController extends Controller
+class TwitterApiController extends ApiController
 {
 
     /**
@@ -80,151 +77,11 @@ class TwitterApiController extends Controller
         return view ('twitter', compact('keyword', 'results', 'sentiments', 'posCoordinates','negCoordinates','neuCoordinates'));
     }
 
-
-// ##### DatumboxAPI sentiment analysis #####
-    private function analyseTweet($results)
-    {
-        require_once('DatumboxAPI.php');
-        $DatumboxAPI = new DatumboxAPI(env('DATUM_BOX_API2'));
-        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
-        //$location = array();
-        $positiveLocation = array();
-        $negativeLocation = array();
-        $neutralLocation = array();
-
-        //echo "<br> DATUMBOX <br>";
-        foreach ($results as $index => $result) {
-            $message = $DatumboxAPI->TwitterSentimentAnalysis(str_replace('@', "", $result['tweet']));
-            //echo "id: " . ($index + 1) . " value: " . $message . "<br>";
-
-            if ($message == "negative") {
-                $sentiment_counter['negative']++;
-            } elseif ($message == "positive") {
-                $sentiment_counter['positive']++;
-            } else {
-                $sentiment_counter['neutral']++;
-            }
-
-
-            // Get lat and long by address
-            $address = $result['user_timezone']; // Google HQ
-            if ($address != null) {
-                $prepAddr = str_replace(' ', '+', $address);
-                $geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . $prepAddr . '&sensor=false');
-                $output = json_decode($geocode);
-                $latitude = $output->results[0]->geometry->location->lat;
-                $longitude = $output->results[0]->geometry->location->lng;
-                //array_push($location,array("lat"=>$latitude,"lng"=>$longitude));
-                if ($message == "negative") {
-                    array_push($negativeLocation, array("lat" => $latitude, "lng" => $longitude));
-                } elseif ($message == "positive") {
-                    array_push($positiveLocation, array("lat" => $latitude, "lng" => $longitude));
-                } else {
-                    array_push($neutralLocation, array("lat" => $latitude, "lng" => $longitude));
-                }
-            }
-        }
-
-        return [$sentiment_counter, $negativeLocation,$positiveLocation,$neutralLocation];
-
-    }
-// ##### AzureAPI sentiment analysis #####
-    private function sentimentAnalysis($results)
-    {
-        $positiveLocation = array();
-        $negativeLocation = array();
-        $neutralLocation = array();
-
-        /* Converting messages into json body for request*/
-        $body_message = '{ "documents" : [';
-        foreach ($results as $index => $result) {
-            $message = $result['tweet'];
-            $message = str_replace('"', "'", $message);
-            $id = $index + 1;
-            $body_message .= '{ "language": "en", 
-                                "id": ' . $id . ',
-                                "text": "' . $message . '"},';
-        }
-        $body_message .= "]}";
-
-        /*Creating a request*/
-        $client = new Client(); //GuzzleHttp\Client
-        $res = $client->request('POST', 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', [
-            'headers' => [
-                'content-type' => 'application/json',
-                'Ocp-Apim-Subscription-Key' => env('AZURE_KEY_2')
-            ],
-            'body' => $body_message
-        ]);
-
-        /*Response returned*/
-        $json = "";
-        if ($res->getStatusCode() == 200) {
-            $json = $res->getBody();
-        }
-
-        /* Parsing results and incrementing a sentiment counter*/
-        $return = json_decode($json, true);
-        $sentiment_counter = array("positive" => 0, "negative" => 0, "neutral" => 0);
-
-        //echo "AZURE" . "<br>";
-        foreach ($return['documents'] as $result) {
-            $score = $result['score'];
-
-            if ($score < 0.5) {
-                $sentiment_counter['negative']++;
-            } elseif ($score > 0.5) {
-                $sentiment_counter['positive']++;
-            } else {
-                $sentiment_counter['neutral']++;
-            }
-
-            // Get lat and long by address
-            $index = $result['id'] - 1;
-            $address = $results[$index]['user_timezone']; // Google HQ
-            if ($address != null) {
-                $prepAddr = str_replace(' ', '+', $address);
-                $geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . $prepAddr . '&sensor=false');
-                $output = json_decode($geocode);
-                $latitude = $output->results[0]->geometry->location->lat;
-                $longitude = $output->results[0]->geometry->location->lng;
-                //array_push($location,array("lat"=>$latitude,"lng"=>$longitude));
-                if ($score < 0.5) {
-                    array_push($negativeLocation, array("lat" => $latitude, "lng" => $longitude));
-                } elseif ($score > 0.5 == "positive") {
-                    array_push($positiveLocation, array("lat" => $latitude, "lng" => $longitude));
-                } else {
-                    array_push($neutralLocation, array("lat" => $latitude, "lng" => $longitude));
-                }
-            }
-
-        }
-
-        return [$sentiment_counter, $negativeLocation,$positiveLocation,$neutralLocation];
-
-    }
-
-    private function saveToCsvFile($results, $filename)
-    {
-        $fp = fopen($filename, 'w');
-        // header for csv
-        $header = [
-            "created_at", "tweet", "user_location", "user_timezone", "geo", "place_coordinates"
-        ];
-
-        fputcsv($fp, $header);
-
-        foreach($results as $result)
-            fputcsv($fp, $result);
-
-        fclose($fp);
-    }
-
     private function extractUsefulFields($result)
     {
         $fields = null;
 
-        // only extra info neededz
+        // only extract info needed
         if( isset($result->created_at) )
             $fields = [
                 'created_at'        => $result->created_at,
@@ -237,26 +94,5 @@ class TwitterApiController extends Controller
             ];
 
         return $fields;
-    }
-
-    private function preprocess($results, $stopwords)
-    {
-        $processedData = [];
-
-        foreach ($results as $result)
-        {
-            $hasStopWord = false;
-
-            foreach ($stopwords as $stopword)
-            {
-                if(strpos(strtolower($result['tweet']), strtolower($stopword)) !== false)
-                    $hasStopWord = true;
-            }
-
-            if(! $hasStopWord )
-                $processedData[] = $result;
-        }
-
-        return $processedData;
     }
 }
