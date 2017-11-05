7e7d7d7a5c9ad785e4a8c70d024819192948cc6e
Grace Zheng <s3558319@student.rmit.edu.au>
2017-11-06 01:49:58 +1100

    fully implemented fb scrapping

--- app/Http/Controllers/FacebookApiController.php
+++ app/Http/Controllers/FacebookApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use Symfony\Component\Process\Process;
 
 class FacebookApiController extends Controller
@@ -13,15 +12,16 @@ class FacebookApiController extends Controller
     public function collect(Request $request)
     {
         // keyword has to be set
-        if ($request->keyword == "") {
+        if ($request->pageId == "") {
             return json_encode([
                 'success' => false,
-                'message' => 'Keyword is required.'
+                'message' => 'Page ID is required.'
             ]);
         }
 
         // extract useful data from request
         $keyword = $request->keyword;
+        $pageId = $request->pageId;
         $count = $request->count > 0 ? $request->count : 100;
         $startingDate = $request->date != "" ?
             Carbon::parse($request->date)->toDateString() :
@@ -29,16 +29,16 @@ class FacebookApiController extends Controller
         $today = Carbon::today()->toDateString();
 
         // execute python script using process, get posts from page entered
-        $process = new Process('python fbScrapper/get_fb_posts_fb_page.py ' . $keyword . ' ' . $startingDate . ' ' . $today);
+        $process = new Process('python fbScrapper/get_fb_posts_fb_page.py ' . $pageId . ' ' . $startingDate . ' ' . $today);
         $process->run();
 
         // execute python script using process, get comments from posts collected
-        $process = new Process('python fbScrapper/get_fb_comments_from_fb.py ' . $keyword . ' ' . $count);
+        $process = new Process('python fbScrapper/get_fb_comments_from_fb.py ' . $pageId . ' ' . $count . ' ' . $keyword);
         $process->run();
 
         return json_encode([
             'success' => true,
-            'path' => asset('/results/facebook_'.$keyword.'.csv')
+            'path' => asset('/results/facebook_' . $keyword . '.csv')
         ]);
     }
 }
